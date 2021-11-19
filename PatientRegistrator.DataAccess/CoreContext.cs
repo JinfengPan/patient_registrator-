93b1ecad081fdf7c7dde3e2c3012432f92dc3a16
Patrick Pan <S6P4@eurofins.com>
2021-11-19 20:12:33 +0000

    create db context

---
+++ PatientRegistrator.DataAccess/CoreContext.cs
@@ -0,0 +1,41 @@
+﻿namespace PatientRegistrator.DataAccess
+{
+    using System.IO;
+
+    using Microsoft.EntityFrameworkCore;
+
+    using PatientRegistrator.Model;
+
+    public class CoreContext : DbContext
+    {
+        public CoreContext(DbContextOptions<CoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //public CoreContext CreateDbContext(string[] args)
+        //{
+        //    var optionsBuilder = new DbContextOptionsBuilder<CoreContext>();
+
+        //    var configuration = new ConfigurationBuilder()
+        //        .SetBasePath(Directory.GetCurrentDirectory())
+        //        .AddJsonFile("appsettings.json")
+        //        .Build();
+
+        //    var connectionString = configuration.GetConnectionString("sqlConnection");
+
+        //    optionsBuilder.UseSqlServer(
+        //        connectionString,
+        //        b => b.MigrationsAssembly("Eurofins.Adms.Core.Infrastructure.Entities.Migrations"));
+
+        //    return new CoreContext(optionsBuilder.Options);
+        //}
+    }
+}
