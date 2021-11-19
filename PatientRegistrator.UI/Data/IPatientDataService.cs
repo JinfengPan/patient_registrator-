a3ed64f6e14d46f64abd9ac5a8c8b5f8e6fe7063
Patrick Pan <S6P4@eurofins.com>
2021-11-19 16:01:55 +0000

    add patient data service

---
+++ PatientRegistrator.UI/Data/IPatientDataService.cs
@@ -0,0 +1,11 @@
+﻿namespace PatientRegistrator.UI.Data
+{
+    using System.Collections.Generic;
+
+    using PatientRegistrator.Model;
+
+    public interface IPatientDataService
+    {
+        IEnumerable<Patient> GetAll();
+    }
+}
---
+++ PatientRegistrator.UI/Data/PatientDataService.cs
@@ -0,0 +1,32 @@
+﻿namespace PatientRegistrator.UI.Data
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using PatientRegistrator.Model;
+
+
+    public class PatientDataService : IPatientDataService
+    {
+        public IEnumerable<Patient> GetAll()
+        {
+            return this._allPatients;
+        }
+
+        private IEnumerable<Patient> _allPatients = new List<Patient>
+                                                        {
+                                                            new Patient
+                                                                {
+                                                                    Name = "张某某",
+                                                                    Age = 37,
+                                                                    Gender = Gender.Male
+                                                                },
+                                                            new Patient
+                                                                {
+                                                                    Name = "王某",
+                                                                    Age = 19,
+                                                                    Gender = Gender.Femail
+                                                                }
+                                                        };
+    }
+}
