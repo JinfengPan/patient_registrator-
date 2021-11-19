8a4a1b8e3bfcd629489b54eefbc92a286e21c596
Patrick Pan <S6P4@eurofins.com>
2021-11-19 16:23:27 +0000

    add view model

---
+++ PatientRegistrator.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace PatientRegistrator.UI.ViewModel
+{
+    using System.Collections.ObjectModel;
+
+    using PatientRegistrator.Model;
+    using PatientRegistrator.UI.Data;
+
+    public class MainViewModel
+    {
+        private IPatientDataService _patientDataService;
+
+        public MainViewModel(IPatientDataService patientDataService)
+        {
+            this._patientDataService = patientDataService;
+            Patients = new ObservableCollection<Patient>();
+        }
+
+        public ObservableCollection<Patient> Patients { get; set; }
+
+        public void Load()
+        {
+            var patients = this._patientDataService.GetAll();
+
+            Patients.Clear();
+
+            foreach (var patient in patients)
+            {
+                Patients.Add(patient);
+            }
+        }
+    }
+}
