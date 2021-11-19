b82187382edceefb17271ed5b9d7d85a0ccd6201
Patrick Pan <S6P4@eurofins.com>
2021-11-19 15:48:06 +0000

    setup scaffold

---
+++ PatientRegistrator.Model/Patient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PatientRegistrator.Model
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public string Hometown { get; set; }
+        public string OnsetTime { get; set; }
+        public string CourseOfDisease { get; set; }
+
+        public bool HasHeadache { get; set; }
+        public bool HasConvulsion { get; set; }
+        public bool HasConsciousnessDisorder { get; set; }
+        public bool HasHemiplegia { get; set; }
+        public bool HasIncontinence { get; set; }
+
+        public string HeadacheDesc { get; set; }
+        public string ConvulsionDesc { get; set; }
+        public string ConsciousnessDisorderDesc { get; set; }
+        public string HemiplegiaDesc { get; set; }
+        public string IncontinenceDesc { get; set; }
+
+        public bool HasUseDrug { get; set; }
+        public string DrugType { get; set; }
+        public string SpecificDrug { get; set; }
+
+        public bool HasSurgery { get; set; }
+        public string SurgeryMethod { get; set; }
+
+        public bool complicationHeadache { get; set; }
+        public bool complicationInfection { get; set; }
+        public bool complicationConvulsion { get; set; }
+        public bool complicationSecondSurgery { get; set; }
+
+        public string FollowUpTime { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male = 1,
+        Femail = 2
+    }
+}
