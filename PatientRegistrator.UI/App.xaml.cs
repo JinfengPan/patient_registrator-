0757185af0ca7a1190b5751507a521ee94bf556d
Patrick Pan <S6P4@eurofins.com>
2021-11-19 17:40:12 +0000

    add IoC

--- PatientRegistrator.UI/App.xaml.cs
+++ PatientRegistrator.UI/App.xaml.cs
@@ -8,10 +8,22 @@ using System.Windows;
 
 namespace PatientRegistrator.UI
 {
+    using Autofac;
+
+    using PatientRegistrator.UI.Startup;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var bootstrapper = new Bootstrapper();
+            var container = bootstrapper.Bootstrap();
+
+            var mainWindow = container.Resolve<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
---
+++ PatientRegistrator.UI/Startup/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿namespace PatientRegistrator.UI.Startup
+{
+    using System.Runtime.InteropServices.ComTypes;
+
+    using Autofac;
+
+    using PatientRegistrator.UI.Data;
+    using PatientRegistrator.UI.ViewModel;
+
+    public class Bootstrapper
+    {
+        public IContainer Bootstrap()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<MainViewModel>().AsSelf();
+            builder.RegisterType<PatientDataService>().As<IPatientDataService>();
+
+            return builder.Build();
+        }
+
+    }
+}
