406dc468880ff08fc6c6a24f4cbfbeb34613b62e
Patrick Pan <S6P4@eurofins.com>
2021-11-21 18:36:20 +0000

    add converter

---
+++ PatientRegistrator.UI/Converters/GenderToNanNvConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace PatientRegistrator.UI.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    using PatientRegistrator.Model;
+
+    public class GenderToNanNvConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Gender val = (Gender)value;
+
+            return val == Gender.Male ? "男" : "女";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
