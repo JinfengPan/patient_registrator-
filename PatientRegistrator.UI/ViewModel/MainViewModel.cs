b20a04da0657aca580bf8fa0a91736789b14d129
Patrick Pan <S6P4@eurofins.com>
2021-11-21 16:25:23 +0000

    implement INotifyPropertyChanged

--- PatientRegistrator.UI/ViewModel/MainViewModel.cs
+++ PatientRegistrator.UI/ViewModel/MainViewModel.cs
@@ -1,14 +1,20 @@
 ﻿namespace PatientRegistrator.UI.ViewModel
 {
     using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
     using System.Threading.Tasks;
 
     using PatientRegistrator.Model;
     using PatientRegistrator.UI.Data;
 
-    public class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
         private IPatientDataService _patientDataService;
+
+        private Patient _selectedPatient;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public MainViewModel(IPatientDataService patientDataService)
         {
@@ -28,6 +34,25 @@
             {
                 Patients.Add(patient);
             }
+        }
+
+        public Patient SelectedPatient
+        {
+            get
+            {
+                return this._selectedPatient;
+            }
+
+            set
+            {
+                this._selectedPatient = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
