b7a0a4a9a7acc2fcc8352544106f44c441ce68f8
Patrick Pan <S6P4@eurofins.com>
2021-11-21 18:29:18 +0000

    add converter

---
+++ PatientRegistrator.UI/Converters/BooleanToShiFouConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace PatientRegistrator.UI.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class BooleanToShiFouConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool val = (bool)value;
+
+            return val ? "是" : "否";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
