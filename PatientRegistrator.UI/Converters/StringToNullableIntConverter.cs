260647eafec5d95d1ed8425cf9232019e7439f7b
Patrick Pan <S6P4@eurofins.com>
2021-11-22 18:21:11 +0000

    add converter

---
+++ PatientRegistrator.UI/Converters/StringToNullableIntConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace PatientRegistrator.UI.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class StringToNullableIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+                              CultureInfo culture)
+        {
+            int? d = (int?)value;
+            if (d.HasValue)
+                return d.Value.ToString(culture);
+            else
+                return String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture)
+        {
+            string s = (string)value;
+            if (String.IsNullOrEmpty(s))
+                return null;
+            else
+                return (int?)int.Parse(s, culture);
+        }
+    }
+}
