0f40739440e2d4f2b02950ff162b710717a7b92a
Patrick Pan <S6P4@eurofins.com>
2021-11-22 18:24:02 +0000

    add converter

---
+++ PatientRegistrator.UI/Converters/StringToNullableDoubleConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace PatientRegistrator.UI.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class StringToNullableDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+                              CultureInfo culture)
+        {
+            double? d = (double?)value;
+            if (d.HasValue)
+                return d.Value.ToString(culture);
+            else
+                return String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture)
+        {
+            string s = (string)value;
+            if (String.IsNullOrEmpty(s))
+                return null;
+            else
+                return (double?)double.Parse(s, culture);
+        }
+    }
+}
