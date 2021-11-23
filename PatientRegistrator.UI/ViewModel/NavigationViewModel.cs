4ecc4bd032a2dc3a764b30a0162dab356c787947
Patrick Pan <S6P4@eurofins.com>
2021-11-23 20:33:02 +0000

    add NavigationViewModel

---
+++ PatientRegistrator.UI/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace PatientRegistrator.UI.ViewModel
+{
+    using System.Collections.ObjectModel;
+    using System.Threading.Tasks;
+
+    using PatientRegistrator.Model;
+    using PatientRegistrator.UI.Data;
+
+
+
+    public class NavigationViewModel : INavigationViewModel
+    {
+        private IPatientDataService _patientDataService;
+
+        public NavigationViewModel(IPatientDataService patientDataService)
+        {
+            this._patientDataService = patientDataService;
+
+            this.Patients = new ObservableCollection<Patient>();
+        }
+
+        public ObservableCollection<Patient> Patients { get; }
+
+        public async Task LoadAsync()
+        {
+            var all = await this._patientDataService.GetAllAsync();
+
+            this.Patients.Clear();
+
+            foreach (var patient in all)
+            {
+                this.Patients.Add(patient);
+            }
+        }
+    }
+}
