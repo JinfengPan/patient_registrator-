aa05d89ca801073f738a1276cbb4dcc88b76a183
Patrick Pan <S6P4@eurofins.com>
2021-11-23 16:15:30 +0000

    update Patient View model

--- PatientRegistrator.UI/ViewModel/PatientViewModel.cs
+++ PatientRegistrator.UI/ViewModel/PatientViewModel.cs
@@ -12,40 +12,17 @@
     {
         private IPatientDataService _patientDataService;
 
-        private Patient _selectedPatient;
+        public Patient Patient { get; set; }
 
-        public PatientViewModel(IPatientDataService patientDataService)
+        public PatientViewModel(Patient patient, IPatientDataService patientDataService)
         {
-            Patients = new ObservableCollection<Patient>();
+            this.Patient = patient;
             this._patientDataService = patientDataService;
         }
 
-        public ObservableCollection<Patient> Patients { get; set; }
-
-        public async Task LoadAsync()
+        public void Save()
         {
-            var patients = await this._patientDataService.GetAllAsync();
-
-            Patients.Clear();
-
-            foreach (var patient in patients)
-            {
-                Patients.Add(patient);
-            }
-        }
-
-        public Patient SelectedPatient
-        {
-            get
-            {
-                return this._selectedPatient;
-            }
-
-            set
-            {
-                this._selectedPatient = value;
-                this.OnPropertyChanged();
-            }
+            this._patientDataService.Save(this.Patient);
         }
     }
 }
