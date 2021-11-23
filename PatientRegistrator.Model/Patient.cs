e93a498931b5acc24394e77a3a57249d2f4451e8
Patrick Pan <S6P4@eurofins.com>
2021-11-23 18:59:01 +0000

    add attribute

--- PatientRegistrator.Model/Patient.cs
+++ PatientRegistrator.Model/Patient.cs
@@ -2,8 +2,11 @@
 
 namespace PatientRegistrator.Model
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Patient
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public int? Age { get; set; }
